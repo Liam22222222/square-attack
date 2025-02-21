c3cc68261340eb1fd2cf5b2eb57a138b93a2e1cd
Liam22222222 <Llail@eastpointsc.org>
2025-02-21 11:19:26 -0500

    v1

---
+++ square-attack.py
@@ -0,0 +1,162 @@
+from pygame.locals import *
+import pygame, sys
+import random
+import math
+
+pygame.init()
+pygame.font.init()
+width = 1600
+hight = 900
+screen = pygame.display.set_mode((width,hight))
+pygame.display.set_caption("Liams game")
+
+point = False
+score = 0
+text = ""
+font = pygame.font.SysFont('Comic Sans MS', 60)
+ply = pygame.Rect(60,60,60,60)
+boxs = []
+boxpos = []
+endx = random.randint(700,1200)
+endy = random.randint(50, 800)
+numbox = random.randint(5, 10)
+joydist = 0.0
+i = 0
+a = 0
+size = 100
+objs = 0.0
+objpos = 0.0
+plyx = 0.0
+plyy = 0.0
+plyxs = 0.0
+plyys = 0.0
+dir = 0.0
+while i < numbox:
+    size = random.randint(100,150)
+    boxs.append(size)
+    boxs.append(size)
+    boxs.append(size)
+    boxs.append(size)
+    boxpos.append(random.randint(-10,1000))
+    boxpos.append(random.randint(-10,500))
+    i += 1
+    
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        plyxs = -5
+    if key[pygame.K_RIGHT]:
+        plyxs = 5
+    if key[pygame.K_DOWN]:
+        plyys = 5
+    if key[pygame.K_UP]:
+        plyys = -5
+        
+    joydist = math.sqrt((plyxs * plyxs) + (plyys * plyys))
+    if joydist > 5:
+        plyxs = plyxs * 0.99
+        plyys = plyys * 0.99
+    plyxs += plyxs * -0.1
+    plyys += plyys * -0.1
+    plyx += plyxs
+    plyy += plyys
+    
+    if plyx > 1700:
+        plyx = 1700
+        plyxs = 0.0
+    if plyx < -100:
+        plyx = -100
+        plyxs = 0.0
+    if plyy > 850:
+        plyy = 850
+        plyys = 0.0
+    if plyy < -100:
+        plyy = -100
+        plyys = 0.0
+    
+    point = ply.collidepoint(endx,endy)
+    if point:
+        ply = pygame.Rect(60,60,60,60)
+        boxs = []
+        boxpos = []
+        endx = random.randint(700,1200)
+        endy = random.randint(50, 800)
+        numbox = random.randint(5, 10)
+        joydist = 0.0
+        i = 0
+        a = 0
+        size = 100
+        objs = 0.0
+        objpos = 0.0
+        plyx = 0.0
+        plyy = 0.0
+        plyxs = 0.0
+        plyys = 0.0
+        score += 1
+        while i < numbox:
+            size = random.randint(100,150)
+            boxs.append(size)
+            boxs.append(size)
+            boxs.append(size)
+            boxs.append(size)
+            boxpos.append(random.randint(-10,1000))
+            boxpos.append(random.randint(-10,500))
+            i += 1
+
+    
+        
+    i = 0
+    a = 0
+    
+    while i < len(boxpos):
+
+        if plyx - 180 > boxpos[i]:
+            boxpos[i] += random.randint(0,5)
+        if plyy - 180 > boxpos[i + 1]:
+            boxpos[i + 1] += random.randint(0,5)
+        if plyx - 180 < boxpos[i]:
+            boxpos[i] += random.randint(-5,0)
+        if plyy - 180 < boxpos[i + 1]:
+            boxpos[i + 1] += random.randint(-5,0)
+
+        if boxpos[i] > 1700:
+            boxpos[i] = 1700
+        if boxpos[i] < -10:
+            boxpos[i] = -10
+        if boxpos[i + 1] > 850:
+            boxpos[i + 1] = 850
+        if boxpos[i + 1] < -100:
+            boxpos[i + 1] = -100
+        i += 2
+
+    text = font.render(str(score), False, (0,0,0))
+
+    screen.fill((127,127,127))
+    pygame.draw.circle(screen,(0,255,0),(endx,endy),60)
+    ply.move_ip(plyxs,plyys)
+    pygame.draw.rect(screen, (255,0,0), ply)
+    i = 0
+    a = 0
+    while i < len(boxs):
+        objs = pygame.Rect(boxs[i], boxs[i + 1], boxs[i + 2], boxs[i + 3])
+        objs.move_ip(boxpos[a], boxpos[a + 1])
+        pygame.draw.rect(screen, (0,0,0), objs)
+        point = objs.collidepoint(plyx,plyy)
+        if point:
+            ply = pygame.Rect(60,60,60,60)
+            plyx = 0.0
+            plyy = 0.0
+            plyxs = 0.0
+            plyys = 0.0
+            score += -1
+        i += 4
+        a += 2
+    screen.blit(text,(10,0))
+    pygame.display.update()
+        
