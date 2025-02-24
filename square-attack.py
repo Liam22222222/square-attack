4f2fbbf9036016824c0ad342ac505fd5b4110ef5
Liam22222222 <Llail@eastpointsc.org>
2025-02-24 11:25:06 -0500

    v1.2

--- square-attack.py
+++ square-attack.py
@@ -18,27 +18,28 @@ time = 5
 level = 1
 lives = 15
 font = pygame.font.SysFont('Comic Sans MS', 60)
-ply = pygame.Rect(60,60,60,60)
 boxs = []
 boxpos = []
-# make these numbers a fraction of width and height, make width and height constants
-# https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python
-endx = random.randint(700,1200)
-endy = random.randint(50, 800)
+endx = random.randint(width - 900,width - 400)
+endy = random.randint(hight - 850, hight - 100)
 numbox = level
 joydist = 0.0
 i = 0
 a = 0
-
-# make a class for a position that holds x, y, size
-# https://www.w3schools.com/python/python_classes.asp
 size = 100
 objs = 0.0
 objpos = 0.0
-plyx = 0.0
-plyy = 0.0
-plyxs = 0.0
-plyys = 0.0
+
+class plyer:
+    def __init__(self,s,x,y,xs,ys):
+        self.s = s
+        self.x = x
+        self.y = y
+        self.xs = xs
+        self.ys = ys
+
+ply = plyer(pygame.Rect(60,60,60,60),0.0,0.0,0.0,0.0)
+
 while i < numbox:
     size = random.randint(100,150)
     boxs.append(size)
@@ -58,44 +59,42 @@ while True:
     
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
-        plyxs = -5
+        ply.xs = -5
     if key[pygame.K_RIGHT]:
-        plyxs = 5
+        ply.xs = 5
     if key[pygame.K_DOWN]:
-        plyys = 5
+        ply.ys = 5
     if key[pygame.K_UP]:
-        plyys = -5
+        ply.ys = -5
         
-    joydist = math.sqrt((plyxs * plyxs) + (plyys * plyys))
+    joydist = math.sqrt((ply.xs * ply.xs) + (ply.ys * ply.ys))
     if joydist > 5:
-        plyxs = plyxs * 0.99
-        plyys = plyys * 0.99
-    plyxs += plyxs * -0.1
-    plyys += plyys * -0.1
-    plyx += plyxs
-    plyy += plyys
+        ply.xs = ply.xs * 0.99
+        ply.ys = ply.ys * 0.99
+    ply.xs += ply.xs * -0.1
+    ply.ys += ply.ys * -0.1
+    ply.x += ply.xs
+    ply.y += ply.ys
     
-    # refer to width
-    if plyx > 1700:
-        plyx = 1700
-        plyxs = 0.0
-    if plyx < -100:
-        plyx = -100
-        plyxs = 0.0
-    if plyy > 850:
-        plyy = 850
-        plyys = 0.0
-    if plyy < -100:
-        plyy = -100
-        plyys = 0.0
+    if ply.x > width + 100:
+        ply.x = width + 100
+        ply.xs = 0.0
+    if ply.x < -100:
+        ply.x = -100
+        ply.xs = 0.0
+    if ply.y > hight - 50:
+        ply.y = hight - 50
+        ply.ys = 0.0
+    if ply.y < -100:
+        ply.y = -100
+        ply.ys = 0.0
     
-    point = ply.collidepoint(endx,endy)
+    point = ply.s.collidepoint(endx,endy)
     if point:
-        ply = pygame.Rect(60,60,60,60)
         boxs = []
         boxpos = []
-        endx = random.randint(700,1200)
-        endy = random.randint(50, 800)
+        endx = random.randint(width - 900,width - 400)
+        endy = random.randint(hight - 850, hight - 100)
         level += 1
         numbox = level
         joydist = 0.0
@@ -104,10 +103,7 @@ while True:
         size = 100
         objs = 0.0
         objpos = 0.0
-        plyx = 0.0
-        plyy = 0.0
-        plyxs = 0.0
-        plyys = 0.0
+        ply = plyer(pygame.Rect(60,60,60,60),0.0,0.0,0.0,0.0)
         score += math.ceil(time)
         time = 5
         while i < numbox:
@@ -120,49 +116,43 @@ while True:
             boxpos.append(random.randint(-10,500))
             i += 1
 
-    
-        
     i = 0
     a = 0
     
     while i < len(boxpos):
 
-        if plyx - 180 > boxpos[i]:
+        if ply.x - 180 > boxpos[i]:
             boxpos[i] += random.randint(0,5)
-        if plyy - 180 > boxpos[i + 1]:
+        if ply.y - 180 > boxpos[i + 1]:
             boxpos[i + 1] += random.randint(0,5)
-        if plyx - 180 < boxpos[i]:
+        if ply.x - 180 < boxpos[i]:
             boxpos[i] += random.randint(-5,0)
-        if plyy - 180 < boxpos[i + 1]:
+        if ply.y - 180 < boxpos[i + 1]:
             boxpos[i + 1] += random.randint(-5,0)
 
-        if boxpos[i] > 1700:
-            boxpos[i] = 1700
+        if boxpos[i] > width + 100:
+            boxpos[i] = width + 100
         if boxpos[i] < -10:
             boxpos[i] = -10
-        if boxpos[i + 1] > 850:
-            boxpos[i + 1] = 850
+        if boxpos[i + 1] > hight - 50:
+            boxpos[i + 1] = hight - 50
         if boxpos[i + 1] < -100:
             boxpos[i + 1] = -100
         i += 2
 
     screen.fill((127,127,127))
     pygame.draw.circle(screen,(0,255,0),(endx,endy),60)
-    ply.move_ip(plyxs,plyys)
-    pygame.draw.rect(screen, (255,0,0), ply)
+    ply.s.move_ip(ply.xs,ply.ys)
+    pygame.draw.rect(screen, (255,0,0), ply.s)
     i = 0
     a = 0
     while i < len(boxs):
         objs = pygame.Rect(boxs[i], boxs[i + 1], boxs[i + 2], boxs[i + 3])
         objs.move_ip(boxpos[a], boxpos[a + 1])
         pygame.draw.rect(screen, (0,0,0), objs)
-        point = objs.collidepoint(plyx,plyy)
+        point = objs.collidepoint(ply.x,ply.y)
         if point:
-            ply = pygame.Rect(60,60,60,60)
-            plyx = 0.0
-            plyy = 0.0
-            plyxs = 0.0
-            plyys = 0.0
+            ply = plyer(pygame.Rect(60,60,60,60),0.0,0.0,0.0,0.0)
             score += -1
         i += 4
         a += 2
