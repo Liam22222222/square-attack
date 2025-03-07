4583207d7199751f5e734de799dfb8479672db92
Liam22222222 <Llail@eastpointsc.org>
2025-03-07 11:35:55 -0500

    not broken

--- projectile.py
+++ projectile.py
@@ -17,6 +17,7 @@ FONT_COLOR = (43,102,158)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Drawing an image and Rotating")
 
+
 # Fonts
 font = pygame.font.SysFont("Arial", 32)
 text = font.render("Projectile", True, FONT_COLOR)
@@ -35,13 +36,12 @@ ball_pos = [SCREEN_WIDTH//2, SCREEN_HEIGHT//2]
 ball_speed = [2,2]
 
 def rot_center(image, angle):
-    """rotate an image while keeping its center and size"""
-    orig_rect = image.get_rect()
-    rot_image = pygame.transform.rotate(image, angle)
-    rot_rect = orig_rect.copy()
-    rot_rect.center = rot_image.get_rect().center
-    rot_image = rot_image.subsurface(rot_rect).copy()
-    return rot_image
+    """rotate a Surface, maintaining position."""
+    
+    loc = image.get_rect().center  #rot_image is not defined 
+    rot_sprite = pygame.transform.rotate(image, angle)
+    rot_sprite.get_rect().center = loc
+    return rot_sprite
 
 projectile = False
 x = SCREEN_WIDTH//2
@@ -57,9 +57,16 @@ while True:
             key = pygame.key.name(event.key)
             projectile = True
             projectile_angle = angle
-    
-
-
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]:
+        img_pos[1] += 2
+    if key[pygame.K_DOWN]:
+        img_pos[1] -= 2
+    if key[pygame.K_LEFT]:
+        img_pos[0] -= 2
+    if key[pygame.K_RIGHT]:
+        img_pos[0] += 2
+    print(key)
     screen.fill(BACKGROUND_COLOR)
     screen.blit(text, (SCREEN_WIDTH // 2 - text.get_width()//2, 100))   # Centering title in the screen
 
@@ -68,8 +75,7 @@ while True:
         y += 4 *math.cos(projectile_angle)
         pygame.draw.circle(screen, BALL_COLOR, (x, y), BALL_RADIUS)
 
-    angle += 1
     rotated_img = rot_center(img, angle)
-    screen.blit(rotated_img,(SCREEN_WIDTH//2,SCREEN_HEIGHT//2))
+    screen.blit(rotated_img,(img_pos[0],img_pos[1]))
     pygame.display.update()
             
